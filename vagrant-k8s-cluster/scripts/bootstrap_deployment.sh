ddad2c8470402aa74941e43625658d593ae508c8
Jibi Jose <jibijose@yahoo.com>
2018-10-31 15:17:17 +0530

    docker version updated

--- vagrant-k8s-cluster/scripts/bootstrap_deployment.sh
+++ vagrant-k8s-cluster/scripts/bootstrap_deployment.sh
@@ -4,6 +4,6 @@ kubectl delete service springwebapp-service
 kubectl delete deployment springwebapp-deployment
 
 kubectl create -f /vagrant/service.yml -f /vagrant/deployment.yml
-#kubectl run --image=registry.hub.docker.com/jibijose/springwebapp springwebapp-deployment --port=8080 --env="DOMAIN=cluster"
+#kubectl run --image=registry.hub.docker.com/jibijose/springwebapp:1.0 springwebapp-deployment --port=8080 --env="DOMAIN=cluster"
 #kubectl expose deployment springwebapp-deployment --port=80 --name=springwebapp-service
 
